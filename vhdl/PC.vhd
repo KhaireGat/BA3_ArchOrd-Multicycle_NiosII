29b4a8caaf22b81a23b7c2f7f370623289031c5e
Nicolas Raulin <nicolas.raulin@epfl.ch>
2019-10-16 12:44:45 +0200

    Control flow added to PC.vdh

--- vhdl/PC.vhd
+++ vhdl/PC.vhd
@@ -17,21 +17,62 @@ entity PC is
 end PC;
 
 architecture synth of PC is
-    signal s_addr: std_logic_vector(31 downto 0);
+    signal s_next,s_addr: std_logic_vector(31 downto 0);
 begin
     
+    
     addr<=s_addr;
-
     process(reset_n,clk)
     begin
         if reset_n='0' then
             s_addr<=(others => '0');
         elsif rising_edge(clk) then
             if en='1' then
-                s_addr(15 downto 0)<= std_logic_vector(unsigned(s_addr(15 downto 0))+to_unsigned(4, 16)); 
+                s_addr<=s_next;
+                --s_next(31 downto 0)<= std_logic_vector(unsigned(s_next(31 downto 0))+to_unsigned(4, 16)); 
 
             end if;
         end if;
     end process;
+
+
+    control_flow : process(s_addr, add_imm, sel_imm, sel_a, imm, a) is
+    begin
+
+            --if (add_imm = '1') then                    -- BRANCH
+            --    s_next <= "0x0000" & std_logic_vector(signed(s_next) + signed(imm));
+
+            --elsif (sel_imm = '1') then                              -- CALL
+            --    s_next <= "0x00000000";
+            --    s_next(17 downto 2) <= imm;
+
+            --elsif (sel_a = '1') then                                -- CALLR
+            --    s_add <= "0x0000" & a;
+
+            --elsif (sel_a = '1') then                                -- JMP
+            --    s_add <= "0x0000" & a;
+
+            --elsif (sel_imm = '1') then                              -- JUMPI
+            --    s_next <= "0x00000000";
+            --    s_next(17 downto 2) <= imm;
+
+        s_next<= std_logic_vector(unsigned(s_addr(31 downto 0))+to_unsigned(4, 32));
+        
+        if (add_imm = '1') then                                 -- BRANCH
+            s_next <= std_logic_vector(signed(s_next) + to_signed(to_integer(signed(imm)), 32));
+
+        elsif (sel_imm = '1') then                              -- CALL & JUMPI (same)
+            s_next(17 downto 2) <= imm;
+
+        elsif (sel_a = '1') then                                -- CALLR & JMP (same)
+            s_next(15 downto 0) <=  a;
+
+
+        end if;
+
+
+        -- normal case? address + 4
+
+    end process control_flow;
 
 end synth;
